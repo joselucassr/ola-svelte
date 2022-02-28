07aeab49a2b2c6ef1094b3442cf78ac374f0d367
José Lucas R. Santos <joselucasrs@gmail.com>
2022-02-28 18:29:39 -0300

    Componente Linha adicionado.

--- src/components/Calculadora.svelte
+++ src/components/Calculadora.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
   import Botao from './Botao.svelte';
+  import Linha from './Linha.svelte';
 </script>
 
 <div class="calculadora">
-  <Botao texto="7" />
-  <Botao texto="8" />
-  <Botao texto="9" />
+  <Linha>
+    <Botao texto="7" />
+    <Botao texto="8" />
+    <Botao texto="9" />
+  </Linha>
+  <Linha>
+    <Botao texto="4" />
+    <Botao texto="5" />
+    <Botao texto="6" />
+  </Linha>
+  <Linha>
+    <Botao texto="1" />
+    <Botao texto="2" />
+    <Botao texto="3" />
+  </Linha>
 </div>
 
 <style>
