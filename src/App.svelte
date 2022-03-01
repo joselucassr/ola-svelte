6bd5e3d9d0eae61091895a07c9157cdece6ab045
José Lucas R. Santos <joselucasrs@gmail.com>
2022-02-28 21:35:39 -0300

    Tentando outra possibilidade.

--- src/App.svelte
+++ src/App.svelte
@@ -10,10 +10,18 @@
   @import url('https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 
   /* Mudando o CSS global */
+  :global(html) {
+    height: 100vh;
+    height: -webkit-fill-available;
+  }
   :global(body) {
     background-color: #282a36;
     padding: 0;
     font-family: 'Ubuntu Mono', monospace;
+    height: 100vh;
+    height: -webkit-fill-available;
+    min-height: 100vh;
+    min-height: -webkit-fill-available;
   }
 
   /* Criando uma variável CSS */
@@ -26,6 +34,9 @@
     justify-content: center;
     align-items: center;
     height: 100vh;
+    height: -webkit-fill-available;
+    min-height: 100vh;
+    min-height: -webkit-fill-available;
   }
 
   /* @media (min-width: 640px) {
